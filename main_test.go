f9c7bda5e4f7a974d7cfbaa9dd578db2446f449b
lucasepe <luca.sepe@gmail.com>
2024-07-24 14:28:33 +0200

    feat: test no name case

--- main_test.go
+++ main_test.go
@@ -10,12 +10,42 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestGreettHandler(t *testing.T) {
+func TestGreetHandlerWithName(t *testing.T) {
 	exp := Greeting{
 		Message: "Hello, Krateo!",
 	}
 
 	req := httptest.NewRequest(http.MethodGet, "/greet?name=Krateo", nil)
+
+	w := httptest.NewRecorder()
+	greetHandler(w, req)
+
+	resp := w.Result()
+	if resp != nil {
+		defer resp.Body.Close()
+	}
+
+	dat, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Errorf("Error: %v", err)
+	}
+
+	got := Greeting{}
+	if err := json.Unmarshal(dat, &got); err != nil {
+		t.Errorf("Error: %v", err)
+	}
+
+	if !cmp.Equal(got, exp) {
+		t.Errorf("got: %v, expected: %v", got, exp)
+	}
+}
+
+func TestGreetHandlerWithNoName(t *testing.T) {
+	exp := Greeting{
+		Message: "Hello, World!",
+	}
+
+	req := httptest.NewRequest(http.MethodGet, "/greet", nil)
 
 	w := httptest.NewRecorder()
 	greetHandler(w, req)
